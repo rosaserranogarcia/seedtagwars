32241b127b66c9b1ddec96726c58f243c7c458c8
rosaserrano <rosaserrano179@gmail.com>
2022-01-25 20:58:02 +0100

    subo proyecto

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src= "/logo-rosa.png" alt="logo" />
+        <section className="App-content">
+
+        </section>
+      </header>
+    </div>
+  );
+}
+
+export default App;
